48766afee9e8b850bbd7b656a4fe4a475fb070b8
MishalRizvi <mishalrizvi37@gmail.com>
2022-12-16 17:52:37 +0000

    Added Admin/UpdateLocation page

--- WebAppsNoAuth/WebAppsNoAuth/Migrations/20221216160459_AddedLocationTable.Designer.cs
+++ WebAppsNoAuth/WebAppsNoAuth/Migrations/20221216160459_AddedLocationTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppsNoAuth.Data;
 
@@ -10,9 +11,11 @@ using WebAppsNoAuth.Data;
 namespace WebAppsNoAuth.Migrations
 {
     [DbContext(typeof(WebAppsNoAuthDbContext))]
-    partial class WebAppsNoAuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221216160459_AddedLocationTable")]
+    partial class AddedLocationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAppsNoAuth/WebAppsNoAuth/Migrations/20221216160459_AddedLocationTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppsNoAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLocationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InstitutionID",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstitutionID",
+                table: "Users");
+        }
+    }
+}
